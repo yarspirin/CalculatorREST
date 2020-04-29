0d978928e52f1f85e8f3c3009e38361d7aef13fe
Yaroslav Spirin <yar.spirin@gmail.com>
2020-04-29 22:26:52 +0300

    Add new webhook

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express();
 
 const PORT = process.env.PORT || 80;
 
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: "Please input expression."
+    });
+});
+
 app.get('/:expression', (req, res) => {
     const expression = req.params["expression"];
     res.status(200).json({
